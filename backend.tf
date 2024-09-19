e72faf3f298e5e4b557c4510f5e3912cdc93da6e
Ebrahimabdelmonem1 <esanad912@gmail.com>
2024-09-19 09:15:00 -0400

    last update

--- backend.tf
+++ backend.tf
@@ -1,24 +1,24 @@
 # Create S3 bucket
-#resource "aws_s3_bucket" "tfstate_bucket" {
-#  bucket = "my-tf-test-bucket-devops"
-#
-#  lifecycle {
-#    prevent_destroy = false
-#  }
+resource "aws_s3_bucket" "tfstate_bucket" {
+  bucket = "my-tf-test-bucket-devops"
 
-#  tags = {
-#    Name        = "My bucket"
-#    Environment = "Dev"
-#  }
-#}
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
 
 # Enable versioning on the S3 bucket
-#resource "aws_s3_bucket_versioning" "Enabled" {
-#  bucket = aws_s3_bucket.tfstate_bucket.id
-#  versioning_configuration {
-#    status = "Enabled"
-#  }
-#}
+resource "aws_s3_bucket_versioning" "Enabled" {
+  bucket = aws_s3_bucket.tfstate_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
 # Create DynamoDB table for state locking
 resource "aws_dynamodb_table" "terraform_locks_tab" {
@@ -39,14 +39,14 @@ resource "aws_dynamodb_table" "terraform_locks_tab" {
 
 
 
-terraform {
-  backend "s3" {
-    bucket         = "my-tf-test-bucket-ebrahim" # Replace with your S3 bucket name
-    key            = "dev/terraform.tfstate"     # Replace with your desired key (path to the state file in the bucket)
-    region         = "us-east-1"                 # Replace with your AWS region
-    dynamodb_table = "my-lock-table"             # Replace with your DynamoDB table name
-    encrypt        = true
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "my-tf-test-bucket-ebrahim" # Replace with your S3 bucket name
+#    key            = "dev/terraform.tfstate"     # Replace with your desired key (path to the state file in the bucket)
+#    region         = "us-east-1"                 # Replace with your AWS region
+#    dynamodb_table = "my-lock-table"             # Replace with your DynamoDB table name
+#    encrypt        = true
+#  }
+#}
 
 
--- main.tf
+++ main.tf
@@ -68,6 +68,63 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 
+#create an elastic IP 1
+resource "aws_eip" "Eip_ngw1" {
+ vpc = true  
+
+tags ={
+ 
+ Name = "first_eip"
+
+}
+
+}
+
+
+#create an elastic IP 2
+resource "aws_eip" "Eip_ngw2" {
+ vpc = true  
+
+tags ={
+ 
+ Name = "second_eip"
+
+}
+
+}
+
+
+
+# Create an NAT gateway 1
+resource "aws_nat_gateway" "my_ngw1" {
+  allocation_id = aws_eip.Eip_ngw1.id
+  subnet_id     = aws_subnet.public_sub1.id
+
+  tags = {
+    Name = "NAT_gw1"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.my_igw]
+}
+
+
+# Create an NAT gateway 2
+resource "aws_nat_gateway" "my_ngw2" {
+  allocation_id = aws_eip.Eip_ngw2.id
+  subnet_id     = aws_subnet.public_sub2.id
+
+  tags = {
+    Name = "NAT_gw2"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.my_igw]
+}
+
+
 # Create a public route table
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.my_VPC.id
@@ -82,22 +139,37 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-#create private aws_route_table
-resource "aws_route_table" "private_route_table" {
+#create private aws_route_table 1
+resource "aws_route_table" "private_route_table1" {
   vpc_id = aws_vpc.my_VPC.id
 
   route {
-    cidr_block = "192.0.0.0/16"
-    gateway_id = "local"
+  #  cidr_block = "192.0.0.0/16"
+  #  gateway_id = "local"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.my_ngw1.id
   }
 
-
-
   tags = {
-    Name = "Private_Route_Table"
+    Name = "Private_Route_Table1"
   }
 }
 
+
+
+#create private aws_route_table 2
+resource "aws_route_table" "private_route_table2" {
+  vpc_id = aws_vpc.my_VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.my_ngw2.id
+  }
+
+  tags = {
+    Name = "Private_Route_Table2"
+  }
+}
 
 
 # Associate the route table with the public subnet
@@ -110,20 +182,20 @@ resource "aws_route_table_association" "public1_rt_association" {
 # Associate the route table with the private subnet
 resource "aws_route_table_association" "private1_rt_association" {
   subnet_id      = aws_subnet.private_sub1.id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.private_route_table1.id
 }
 
 
 
-# Associate the route table with the public subnet
+# Associate the route table with the public subnet 2
 resource "aws_route_table_association" "public2_rt_association" {
   subnet_id      = aws_subnet.public_sub2.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 
-# Associate the route table with the private subnet
+# Associate the route table with the private subnet 2
 resource "aws_route_table_association" "private2_rt_association" {
   subnet_id      = aws_subnet.private_sub2.id
-  route_table_id = aws_route_table.private_route_table.id
+  route_table_id = aws_route_table.private_route_table2.id
 }
